14943482b205657336611683018d34ab2da08ee7
Your NameMichelle <watts.e.michelle@gmail.com>
2021-07-08 18:06:55 +1000

    manifest and service worker added

---
+++ public/service-worker.js
@@ -0,0 +1,28 @@
+
+// install event handler
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('static').then( cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './styles.css',
+        './index.js',
+        './icons/icon-192x192.png',
+        './icons/icon-512x512.png',
+        './manifest.webmanifest',
+      ]);
+    })
+  );
+  console.log('Install');
+  self.skipWaiting();
+});
+
+// retrieve assets from cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then( response => {
+      return response || fetch(event.request);
+    })
+  );
+});
